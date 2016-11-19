4f0497e8b76ca228043dbae443502efff126755c
Udit Vira <udit.vira@gmail.com>
2016-11-19 17:29:47 -0500

    Exposed on_receive_from for bridge

--- SteamLink/SteamLink.cpp
+++ SteamLink/SteamLink.cpp
@@ -183,15 +183,14 @@ void SteamLink::debug(char* string) {
   return;
 };
 
-
-void SteamLink::phex(uint8_t *data, unsigned int length) // prints 8-bit data in hex with leading zeroes
-{
-       Serial.print("0x"); 
-       for (int i=0; i<length; i++) { 
-         if (data[i]<0x10) {Serial.print("0");} 
-         Serial.print(data[i],HEX); 
-         Serial.print(" "); 
-       }
+// prints 8-bit data in hex with leading zeroes
+void SteamLink::phex(uint8_t *data, unsigned int length) {
+  Serial.print("0x"); 
+  for (int i=0; i<length; i++) { 
+    if (data[i]<0x10) {Serial.print("0");} 
+    Serial.print(data[i],HEX); 
+    Serial.print(" "); 
+  }
 }
 
 
