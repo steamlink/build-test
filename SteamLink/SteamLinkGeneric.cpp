fdc2a16bc1a3a4e89e554224dee115e3e7c9f840
Andreas Wrede <andreas@wrede.ca>
2017-06-11 16:09:19 -0400

    more debug statements

--- SteamLink/SteamLinkGeneric.cpp
+++ SteamLink/SteamLinkGeneric.cpp
@@ -63,6 +63,9 @@ bool SteamLinkGeneric::send_ds(uint8_t* payload, uint8_t payload_length) {
   header.qos = SL_DEFAULT_QOS;
   packet_length = SteamLinkPacket::set_packet(packet, payload, payload_length, (uint8_t*) &header, sizeof(header));
   bool sent = driver_send(packet, packet_length, SL_DEFAULT_STORE_ADDR, false);
+  INFO("SteamLinkGeneric::send_ds packet: ");
+  phex(packet, packet_length);
+  INFONL();
   free(packet);
   return sent;
 }
@@ -77,6 +80,9 @@ bool SteamLinkGeneric::send_bs(uint8_t* payload, uint8_t payload_length) {
   header.qos = SL_DEFAULT_QOS;
   packet_length = SteamLinkPacket::set_packet(packet, payload, payload_length, (uint8_t*) &header, sizeof(header));
   bool sent = driver_send(packet, packet_length, SL_DEFAULT_STORE_ADDR, false);
+  INFO("SteamLinkGeneric::send_bs packet: ");
+  phex(packet, packet_length);
+  INFONL();
   free(packet);
   return sent;
 }
@@ -89,6 +95,9 @@ bool SteamLinkGeneric::send_on() {
   header.slid = _slid;
   packet_length = SteamLinkPacket::set_packet(packet, NULL, 0, (uint8_t*) &header, sizeof(header));
   bool sent = driver_send(packet, packet_length, SL_DEFAULT_STORE_ADDR, false);
+  INFO("SteamLinkGeneric::send_on packet: ");
+  phex(packet, packet_length);
+  INFONL();
   free(packet);
   return sent;
 }
@@ -101,6 +110,9 @@ bool SteamLinkGeneric::send_ak() {
   header.slid = _slid;
   packet_length = SteamLinkPacket::set_packet(packet, NULL, 0, (uint8_t*) &header, sizeof(header));
   bool sent = driver_send(packet, packet_length, SL_DEFAULT_STORE_ADDR, false);
+  INFO("SteamLinkGeneric::send_ak packet: ");
+  phex(packet, packet_length);
+  INFONL();
   free(packet);
   return sent;
 }
@@ -113,6 +125,9 @@ bool SteamLinkGeneric::send_nk() {
   header.slid = _slid;
   packet_length = SteamLinkPacket::set_packet(packet, NULL, 0, (uint8_t*) &header, sizeof(header));
   bool sent = driver_send(packet, packet_length, SL_DEFAULT_STORE_ADDR, false);
+  INFO("SteamLinkGeneric::send_nk packet: ");
+  phex(packet, packet_length);
+  INFONL();
   free(packet);
   return sent;
 }
@@ -126,6 +141,9 @@ bool SteamLinkGeneric::send_tr(uint8_t* payload, uint8_t payload_length) {
   header.rssi = _last_rssi;
   packet_length = SteamLinkPacket::set_packet(packet, payload, payload_length, (uint8_t*) &header, sizeof(header));
   bool sent = driver_send(packet, packet_length, SL_DEFAULT_STORE_ADDR, false);
+  INFO("SteamLinkGeneric::send_tr packet: ");
+  phex(packet, packet_length);
+  INFONL();
   free(packet);
   return sent;
 }
@@ -138,6 +156,9 @@ uint8_t packet_length;
   header.slid = _slid;
   packet_length = SteamLinkPacket::set_packet(packet, payload, payload_length, (uint8_t*) &header, sizeof(header));
   bool sent = driver_send(packet, packet_length, SL_DEFAULT_STORE_ADDR, false);
+  INFO("SteamLinkGeneric::send_ss packet: ");
+  phex(packet, packet_length);
+  INFONL();
   free(packet);
   return sent;
 }
@@ -153,7 +174,9 @@ void SteamLinkGeneric::handle_admin_packet(uint8_t* packet, uint8_t packet_lengt
     if (_on_receive != NULL) {
       _on_receive(payload, payload_length);
     }
-    INFONL("Handled packet, freeing memory");
+    INFO("SteamLinkGeneric::handle_admin_packet DN  packet: ");
+    phex(pkt_header, packet_length);
+    INFONL();
     free(pkt_header);
 
   } else if (op == SL_OP_BN) {
@@ -161,8 +184,8 @@ void SteamLinkGeneric::handle_admin_packet(uint8_t* packet, uint8_t packet_lengt
     uint8_t* pkt_header;
     uint8_t* payload;
     uint8_t payload_length = SteamLinkPacket::get_packet(packet, packet_length, payload, pkt_header, (uint8_t) sizeof(bn_header));
-    INFO("Printing header: ");
-    INFONL(*pkt_header);
+    INFO("Printing pkg_header: ");
+    Serial.println((uint32_t) pkt_header, HEX);
     // if possible, send it up to the bridge level
     if (is_physical) {
       _bridge_handler(payload, payload_length, ((bn_header*) pkt_header)->slid);
@@ -170,7 +193,11 @@ void SteamLinkGeneric::handle_admin_packet(uint8_t* packet, uint8_t packet_lengt
       // we need to forward it out the physical layer
       driver_send(payload, payload_length, ((bn_header*) pkt_header)->slid, false);
     }
-    INFONL("Handled packet, freeing memory");
+    INFO("SteamLinkGeneric::handle_admin_packet BN  packet: ");
+    phex(pkt_header, packet_length);
+    INFONL();
+    INFO("Printing pkg_header again: ");
+    Serial.println((uint32_t) pkt_header, HEX);
     free(pkt_header);
 
   } else if (op == SL_OP_GS) {
