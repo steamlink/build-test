eef4a3661bf11db3759a519c07fa95cfdc60cd77
Andreas Wrede <andreas@wrede.ca>
2017-06-14 09:59:14 -0400

    improve phex

--- SteamLink/SteamLink.cpp
+++ SteamLink/SteamLink.cpp
@@ -1,10 +1,30 @@
 #include <SteamLink.h>
 
 void phex(uint8_t *data, unsigned int length) {
-  Serial.print("0x");
+  char sbuf[17]; 
+  char hbuf[49]; 
+  uint8_t ih = 0;
+
+  hbuf[48] = '\0';
+  sbuf[16] = '\0';
   for (int i=0; i<length; i++) {
-    if (data[i]<0x10) {Serial.print("0");}
-    Serial.print(data[i],HEX);
-    Serial.print(" ");
+    sprintf(&hbuf[ih*3], "%02x ", (char) data[i]);
+    if ((data[i] >= 0x20) && (data[i] <= 0x7f)) {
+        sbuf[ih] = (char) data[i];
+    } else {
+        sbuf[ih] = '.';
+    }
+    ih += 1;
+    if ((i % 16) == 15) {
+        Serial.print(hbuf);
+        Serial.println(sbuf);
+        ih = 0;
+    }
+  }
+  if (ih > 0) {
+      sbuf[ih] = '\0';
+      hbuf[ih*3] = '\0';
+      Serial.print(hbuf);
+      Serial.println(sbuf);
   }
 }
